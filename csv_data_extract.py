44d21be337bc804a6127f9fdab1ab1696cd8e558
Kjell Keune <k.keune@tu-braunschweig.de>
2024-01-02 16:49:19 +0100

    clean up data

--- csv_data_extract.py
+++ csv_data_extract.py
@@ -10,11 +10,21 @@ if __name__=="__main__":
     path_in = os.path.join("./data", args.inputfile)
     path_out = os.path.join("./data_extract", args.outputfile)
     data = {}
+    allowed_clubs = set()
     # read cvs file
     with open(path_in) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            transfer = (row["club_name"],row["club_involved_name"])
+            allowed_clubs.add(row["club_name"])
+            # only known clubs
+            if row["club_involved_name"] == "Unknown":
+                continue
+            # determine direction of transfer
+            if row["transfer_movement"] == "out":
+                transfer = (row["club_name"],row["club_involved_name"])
+            elif row["transfer_movement"] == "in":
+                transfer = (row["club_involved_name"], row["club_name"])
+            # add transfer to data
             if not transfer in data:
                 data[transfer] = 1
             else:
@@ -22,7 +32,9 @@ if __name__=="__main__":
     # write data to cvs file
     with open(path_out, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["club_name","club_involved_name","transfers"])
+        writer.writerow(["club_from","club_to","transfers"])
         for item in data.items():
+            if not item[0][1] in allowed_clubs or not item[0][0] in allowed_clubs:
+                continue
             writer.writerow([item[0][0], item[0][1], item[1]])
     print(f"Data written to {path_out}")
