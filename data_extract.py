5c90b20fa21c8a815d5143bc701d5c5ed2ac6420
Kjell Keune <k.keune@tu-braunschweig.de>
2024-01-11 11:28:13 +0100

    added subgraphs

---
+++ data_extract.py
@@ -0,0 +1,38 @@
+import os
+import csv
+
+def extract_league(league, from_year, to_year):
+    clubs_trans = {}
+    clubs_in_league = set()
+    path = os.path.join("data", league) + ".csv"
+    # read cvs file and extract data
+    with open(path) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            clubs_in_league.add(row["club_name"])
+            # only known clubs
+            if row["club_involved_name"] == "Unknown":
+                continue
+            # timespan
+            if not (from_year <= int(row["year"]) <= to_year):
+                continue 
+            # determine direction of transfer
+            if row["transfer_movement"] == "out":
+                transfer = (row["club_name"],row["club_involved_name"])
+            elif row["transfer_movement"] == "in":
+                transfer = (row["club_involved_name"], row["club_name"])
+            # add transfer to data
+            if not transfer in clubs_trans:
+                clubs_trans[transfer] = 1
+            else:
+                clubs_trans[transfer] += 1
+    # setup list and split data
+    in_league = []
+    out_league = []
+    for item in clubs_trans.items():
+        row = {"club_from":item[0][0], "club_to":item[0][1], "transfers":item[1]}
+        if item[0][0] in clubs_in_league and item[0][1] in clubs_in_league:
+            in_league.append(row)
+        else:
+            out_league.append(row)
+    return in_league, out_league
